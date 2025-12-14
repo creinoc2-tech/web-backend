e2a1187ea434239489ff4c4995b604b55f16ee55
user <Christian>
2025-12-13 20:33:55 -0500

    dotenv

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
+const dotenv = require('dotenv').config()
+
 const app = express()
-const port = 3001
+const port = process.env.PORT 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+}) 
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+}) 
